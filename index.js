33f4caed26c417baaa610e0b9454c0aa99f3fff1
Fred <fdlai@yahoo.com>
2023-04-04 08:37:43 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+    if (event.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    const nums = parseInt(dodger.style.left.replace('px', ''), 10);
+    if (nums >= 2) {
+        dodger.style.left = `${nums - 2}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const nums = parseInt(dodger.style.left.replace('px', ''), 10);
+    if (nums <= 358) {
+        dodger.style.left = `${nums + 2}px`;
+    }
+}
